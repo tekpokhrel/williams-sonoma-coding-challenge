68d86bafefd7c517fe9401515e96e91b25038556
Tek Pokhrel <teknath.pokharel86@gmail.com>
2018-01-11 00:30:47 -0600

    Displayed product with hero image, title, price, and carousel

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import {ProductService} from './services/product.service';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
